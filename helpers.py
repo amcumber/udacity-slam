948d763ecdb16a62d5ad53550a61b7d962cc56ad
Aaron McUmber, Ph.D <aaron.mcumber@gmail.com>
2021-12-13 21:02:11 -0800

    First pass at dist func - needs fix

--- helpers.py
+++ helpers.py
@@ -11,59 +11,84 @@ import seaborn as sns
 # it assumes the world is a square grid of some given size
 # and that landmarks is a list of landmark positions(an optional argument)
 def display_world(world_size, position, landmarks=None):
-    
+
     # using seaborn, set background grid to gray
     sns.set_style("dark")
 
     # Plot grid of values
-    world_grid = np.zeros((world_size+1, world_size+1))
+    world_grid = np.zeros((world_size + 1, world_size + 1))
 
     # Set minor axes in between the labels
-    ax=plt.gca()
-    cols = world_size+1
-    rows = world_size+1
+    ax = plt.gca()
+    cols = world_size + 1
+    rows = world_size + 1
 
-    ax.set_xticks([x for x in range(1,cols)],minor=True )
-    ax.set_yticks([y for y in range(1,rows)],minor=True)
-    
+    ax.set_xticks([x for x in range(1, cols)], minor=True)
+    ax.set_yticks([y for y in range(1, rows)], minor=True)
+
     # Plot grid on minor axes in gray (width = 1)
-    plt.grid(which='minor',ls='-',lw=1, color='white')
-    
+    plt.grid(which="minor", ls="-", lw=1, color="white")
+
     # Plot grid on major axes in larger width
-    plt.grid(which='major',ls='-',lw=2, color='white')
-    
+    plt.grid(which="major", ls="-", lw=2, color="white")
+
     # Create an 'o' character that represents the robot
     # ha = horizontal alignment, va = vertical
-    ax.text(position[0], position[1], 'o', ha='center', va='center', color='r', fontsize=30)
-    
+    ax.text(
+        position[0], position[1], "o", ha="center", va="center", color="r", fontsize=30
+    )
+
     # Draw landmarks if they exists
-    if(landmarks is not None):
+    if landmarks is not None:
         # loop through all path indices and draw a dot (unless it's at the car's location)
-        for pos in landmarks:
-            if(pos != position):
-                ax.text(pos[0], pos[1], 'x', ha='center', va='center', color='purple', fontsize=20)
-    
+        for landmark in landmarks:
+            # [ACM] changed landmark definition - need to fix this interaction
+            pos = landmark.x, landmark.y
+            if pos != position:
+                ax.text(
+                    pos[0],
+                    pos[1],
+                    "x",
+                    ha="center",
+                    va="center",
+                    color="purple",
+                    fontsize=20,
+                )
+
     # Display final result
     plt.show()
 
-    
+
 # --------
 # this routine makes the robot data
 # the data is a list of measurements and movements: [measurements, [dx, dy]]
 # collected over a specified number of time steps, N
 #
-def make_data(N, num_landmarks, world_size, measurement_range, motion_noise, 
-              measurement_noise, distance):
+def make_data(
+    N,
+    num_landmarks,
+    world_size,
+    measurement_range,
+    motion_noise,
+    measurement_noise,
+    distance,
+):
 
     # check that data has been made
     try:
-        check_for_data(num_landmarks, world_size, measurement_range, motion_noise, measurement_noise)
+        check_for_data(
+            num_landmarks,
+            world_size,
+            measurement_range,
+            motion_noise,
+            measurement_noise,
+        )
     except ValueError:
-        print('Error: You must implement the sense function in robot_class.py.')
+        print("Error: You must implement the sense function in robot_class.py.")
         return []
-    
+
     complete = False
-    
+
     r = robot(world_size, measurement_range, motion_noise, measurement_noise)
     r.make_landmarks(num_landmarks)
 
@@ -72,21 +97,21 @@ def make_data(N, num_landmarks, world_size, measurement_range, motion_noise,
         data = []
 
         seen = [False for row in range(num_landmarks)]
-    
+
         # guess an initial motion
         orientation = random.random() * 2.0 * pi
         dx = cos(orientation) * distance
         dy = sin(orientation) * distance
-            
-        for k in range(N-1):
-    
+
+        for k in range(N - 1):
+
             # collect sensor measurements in a list, Z
             Z = r.sense()
 
-            # check off all landmarks that were observed 
+            # check off all landmarks that were observed
             for i in range(len(Z)):
                 seen[Z[i][0]] = True
-    
+
             # move
             while not r.move(dx, dy):
                 # if we'd be leaving the robot world, pick instead a new direction
@@ -98,23 +123,23 @@ def make_data(N, num_landmarks, world_size, measurement_range, motion_noise,
             data.append([Z, [dx, dy]])
 
         # we are done when all landmarks were observed; otherwise re-run
-        complete = (sum(seen) == num_landmarks)
+        complete = sum(seen) == num_landmarks
 
-    print(' ')
-    print('Landmarks: ', r.landmarks)
+    print(" ")
+    print("Landmarks: ", r.landmarks)
     print(r)
-
 
     return data
 
 
-def check_for_data(num_landmarks, world_size, measurement_range, motion_noise, measurement_noise):
+def check_for_data(
+    num_landmarks, world_size, measurement_range, motion_noise, measurement_noise
+):
     # make robot and landmarks
     r = robot(world_size, measurement_range, motion_noise, measurement_noise)
     r.make_landmarks(num_landmarks)
-    
-    
+
     # check that sense has been implemented/data has been made
     test_Z = r.sense()
-    if(test_Z is None):
+    if test_Z is None:
         raise ValueError
